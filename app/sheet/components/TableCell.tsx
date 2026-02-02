52ae002daac456e58855d9b080ee0697e0653ad4
oyo <bemyangel2@gmail.com>
2026-02-03 05:41:44 +0900

    style: sheet table style

--- app/sheet/components/TableCell.tsx
+++ app/sheet/components/TableCell.tsx
@@ -6,7 +6,7 @@ interface TableCellProps {
 
 const TableCell = ({ children }: TableCellProps) => {
 	return (
-		<div className="border border-r-0 border-t-0 px-2 py-4 flex flex-1 justify-center items-center text-center">
+		<div className="outline-1 outline-gray-200 -outline-offset-1 px-2 py-4 flex flex-1 justify-center items-center text-center">
 			{children}
 		</div>
 	)
--- app/sheet/components/TableRow.tsx
+++ app/sheet/components/TableRow.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from 'react'
 interface TableRowProps {
 	headText: string
 	children: ReactNode
+	className?: string
 }
 
-const TableRow = ({ headText, children }: TableRowProps) => {
+const TableRow = ({ headText, children, className }: TableRowProps) => {
 	return (
-		<div className="flex border border-t-0">
-			<div className="px-5 py-4 w-36">
+		<div className="flex">
+			<div className="outline-1 outline-gray-200 -outline-offset-1 px-5 py-4 w-36">
 				<h5 className="font-bold">{headText}</h5>
 			</div>
 
